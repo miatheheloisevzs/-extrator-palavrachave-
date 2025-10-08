f47e2afa5ad6fa1518b9a3f3468345d8b60adf79
maithe heloise <maithe.faria@escola.pr.gov.br>
2025-10-08 08:13:57 -0300

    salvar

---
+++ script.js
@@ -0,0 +1,15 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
+function mostraPalavrasChave() { 
+  alert("fui clicado!"); 
+} 
+
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave"); 
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
+
+function mostraPalavrasChave() { 
+  alert("fui clicado!"); 
+} 
