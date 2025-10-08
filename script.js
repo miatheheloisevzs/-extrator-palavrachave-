debec7a309622890ebe3a76d1e0482245030ec05
maithe heloise <maithe.faria@escola.pr.gov.br>
2025-10-08 08:23:01 -0300

    salvar

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-  const texto = document.querySelector("#entrada-de-texto").value;
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
 
-  const campoResultado = document.querySelector("#resultado-palavrachave");
-
-  const palavras = texto.split(" ");
-
-  campoResultado.textContent = palavras;
+    campoResultado.textContent = palavras.join(", ");
 }
